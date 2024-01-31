9a360ec0ba9dca3918fc783a1e6cdcf5bf8d0da5
gaveeshDilsara <gaveeshrcg@gmail.com>
2024-01-31 17:05:28 +0530

    hello

--- car_project.ino
+++ car_project.ino
@@ -2,71 +2,54 @@ char in1 = 6;
 char in2 = 7;
 char in3 = 8;
 char in4 = 9;
+char in5 = 5;
+char in6 = 3;
 
-void goForward(void);
-void goBackward(void);
-void turnLeft(void);
-void turnRight(void);
-void stop(int delayTime);
+
+unsigned char mapped1;
+unsigned char mapped2;
+
+
+void goForward1(unsigned char speed);
+void goForward2(unsigned char speed);
+
 
 void setup() {
-    pinMode(in1,OUTPUT);
-    pinMode(in2,OUTPUT);
-    pinMode(in3,OUTPUT);
-    pinMode(in4,OUTPUT);
+pinMode(in1,OUTPUT);
+pinMode(in2,OUTPUT);
+pinMode(in3,OUTPUT);
+pinMode(in4,OUTPUT);
+pinMode(in5,OUTPUT);
+pinMode(in6,OUTPUT);
+
+
+mapped1 = map(6,0,10,0,255);
+mapped2 = map(7,0,10,0,255);
 }
 
 void loop() {
-    
-    goForward();
-    delay(1000);
-    stop(250);
+goForward1(mapped1);
+goForward2(mapped2);
+delay(1000);
+stop();
 
-    goBackward();
-    delay(1000);
-    stop(250);
 
-    turnRight();
-    delay(1000);
-    stop(250);
-
-    turnLeft();
-    delay(1000);
-    stop(500);
 }
 
-void goForward(void){
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,HIGH);
-    digitalWrite(in3,LOW);
-    digitalWrite(in4,HIGH);
+void goForward1(unsigned char speed){
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  analogWrite(in6,speed);
+}
+void goForward2(unsigned char speed){
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+  analogWrite(in5,speed);
 }
 
-void goBackward(void){
-    digitalWrite(in1,HIGH);
-    digitalWrite(in2,LOW);
-    digitalWrite(in3,HIGH);
-    digitalWrite(in4,LOW);
-}
-
-void turnRight(void){
-    digitalWrite(in1,HIGH);
-    digitalWrite(in2,LOW);
-    digitalWrite(in3,LOW);
-    digitalWrite(in4,HIGH);
-}
-
-void turnLeft(void){
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,HIGH);
-    digitalWrite(in3,HIGH);
-    digitalWrite(in4,LOW);
-}
-
-void stop(int delayTime){
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,LOW);
-    digitalWrite(in3,LOW);
-    digitalWrite(in4,LOW);
-    delay(delayTime);
+void stop(void){
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,LOW);
 }
