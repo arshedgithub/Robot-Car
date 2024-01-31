6a79fe574be643e8b5b1ab9187d7d5c559c2fe70
arshedgithub <arshedahmedmail@gmail.com>
2024-01-31 16:14:28 +0530

    adding main functions

--- car_project.ino
+++ car_project.ino
@@ -3,30 +3,70 @@ char in2 = 7;
 char in3 = 8;
 char in4 = 9;
 
-void goForward();
-void goBackward();
-void turnRight();
-void turnLeft();
-void stop();
+void goForward(void);
+void goBackward(void);
+void turnLeft(void);
+void turnRight(void);
+void stop(int delayTime);
 
-void setup(){
-    pinMode(in1, OUTPUT);
-    pinMode(in2, OUTPUT);
-    pinMode(in3, OUTPUT);
-    pinMode(in4, OUTPUT);
+void setup() {
+    pinMode(in1,OUTPUT);
+    pinMode(in2,OUTPUT);
+    pinMode(in3,OUTPUT);
+    pinMode(in4,OUTPUT);
 }
 
-void loop(){
+void loop() {
+    
+    goForward();
+    delay(1000);
+    stop(250);
 
+    goBackward();
+    delay(1000);
+    stop(250);
+
+    turnRight();
+    delay(1000);
+    stop(250);
+
+    turnLeft();
+    delay(1000);
+    stop(500);
 }
 
+void goForward(void){
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+    digitalWrite(in3,LOW);
+    digitalWrite(in4,HIGH);
+}
 
-void goForward(){}
+void goBackward(void){
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+    digitalWrite(in3,HIGH);
+    digitalWrite(in4,LOW);
+}
 
-void goBackward(){}
+void turnRight(void){
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+    digitalWrite(in3,LOW);
+    digitalWrite(in4,HIGH);
+}
 
-void turnRight(){}
+void turnLeft(void){
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+    digitalWrite(in3,HIGH);
+    digitalWrite(in4,LOW);
+}
 
-void turnLeft(){}
-
-void stop(){}
+void stop(int delayTime){
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,LOW);
+    digitalWrite(in3,LOW);
+    digitalWrite(in4,LOW);
+    delay(delayTime);
+}
