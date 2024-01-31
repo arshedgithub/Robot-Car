1f4a771511f27a5b464e37ef00fdd75f9a16953f
arshedgithub <arshedahmedmail@gmail.com>
2024-01-31 16:30:36 +0530

    handle other errors

--- remote.py
+++ remote.py
@@ -2,7 +2,7 @@ import serial
 import time
 
 # connect to arduino via serial ( COM port may vary )
-arduino = serial.Serial(port='COM4', baudrate=9600, timeout=.1)
+arduino = serial.Serial(port='COM8', baudrate=9600, timeout=.1)
 
 # 2 seconds to settle up the connection
 time.sleep(2) 
@@ -12,6 +12,9 @@ try:
 
 except KeyboardInterrupt:
     print("Programme terminated...")
+
+except:
+    print("Something Wrong...")
 
 finally:
     arduino.close()
