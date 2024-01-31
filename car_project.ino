a0f90dda94b463bf3291533d6d587a8f0f9108cd
arshedgithub <arshedahmedmail@gmail.com>
2024-01-31 15:49:51 +0530

    initializing project

---
+++ car_project.ino
@@ -0,0 +1,32 @@
+char in1 = 6;
+char in2 = 7;
+char in3 = 8;
+char in4 = 9;
+
+void goForward();
+void goBackward();
+void turnRight();
+void turnLeft();
+void stop();
+
+void setup(){
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
+}
+
+void loop(){
+
+}
+
+
+void goForward(){}
+
+void goBackward(){}
+
+void turnRight(){}
+
+void turnLeft(){}
+
+void stop(){}
